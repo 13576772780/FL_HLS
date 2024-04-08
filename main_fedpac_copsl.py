164944ae788fbd762fdb9d1cd8a22e9bdcdef912
13576772780 <1978335752@qq.com>
2024-04-08 14:33:27 +0800

    modify code

--- main_fedpac_copsl.py
+++ main_fedpac_copsl.py
@@ -343,7 +343,7 @@ if __name__ == '__main__':
                 accs10 += acc_test/10
 
             # below prints the global accuracy of the single global model for the relevant algs
-            if args.alg == 'fedavg' or args.alg == 'prox' or True:
+            if args.alg == 'fedavg' or args.alg == 'prox' or args.alg == 'fedrep':
                 acc_test, loss_test = test_img_local_all(net_glob, args, dataset_test, dict_users_test,
                                                         w_locals=None,indd=indd,dataset_train=dataset_train, dict_users_train=dict_users_train, return_all=False, concept_matrix=concept_matrix)
                 if iter != args.epochs:
